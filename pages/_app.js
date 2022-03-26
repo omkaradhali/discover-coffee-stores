c99f55c86f3890bb5e2121ab4d8acc0bb4544052
Adhali <adhalio@lpth0234.lan>
2022-03-26 19:17:32 -0400

    adding global footer and sample hello world page

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+  <div>
+    <Component {...pageProps} />
+    <footer>
+      <p>&copy; 2022 Omkar Adhali</p>
+    </footer>
+  </div>)
 }
 
 export default MyApp
