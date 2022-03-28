9f535ae01e0ba5df4b8045343be0a83dfdbca845
Omkar Adhali <omkar.a2989@gmail.com>
2022-03-28 08:44:30 -0400

    adding dynamic routes

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,10 +1,14 @@
 import React from 'react';
-
+import { useRouter } from 'next/router';
 
 const CoffeeStore = () => {
+
+  const router = useRouter()
+
+  console.log(router.query.id)
   return (
     <div>
-      <p>Coffee Store page</p>
+      <p>Coffee Store page at location {router.query.id}</p>
     </div>
   )
 }
