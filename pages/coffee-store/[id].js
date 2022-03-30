111fe1e450ea10b95bcaa43cc646eaa6771ee0c7
Omkar Adhali <omkar.a2989@gmail.com>
2022-03-30 17:28:24 -0400

    static and dynamic routing in NextJS

--- pages/coffee-store/[id].js
+++ pages/coffee-store/[id].js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { useRouter } from 'next/router';
+import Link from 'next/link';
 
 const CoffeeStore = () => {
 
@@ -9,6 +10,12 @@ const CoffeeStore = () => {
   return (
     <div>
       <p>Coffee Store page at location {router.query.id}</p>
+      <Link href="/">
+        <a>Back to Home</a>
+      </Link>
+      <Link href="/coffee-store/dynamic">
+        <a>Go to page dynamic</a>
+      </Link>
     </div>
   )
 }
